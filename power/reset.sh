50f4b5a7f1eb0b391fb10756af6e7f1b5caa4cfd
BluntForceOps <204597971+BluntForceOps@users.noreply.github.com>
2025-07-04 22:43:40 +0200

    Unify testsetup control

--- power/reset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/.ha_creds.sh
-
-curl -X POST -H "Authorization: Bearer ${HA_TOKEN}" -H "Content-Type: application/json" -d '{"entity_id": "switch.amdtestcontrol_reset_pin"}' ${HA_URL}/api/services/switch/turn_on
